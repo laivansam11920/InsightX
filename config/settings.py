d04190c4fa0a55b00dc8e57b56441314396975eb
Lai Van Sam <samvasang1192011@gmail.com>
2026-07-17 15:51:43 +0700

    feat: add run.py and install.sh and .gitkeep

--- config/settings.py
+++ config/settings.py
@@ -11,13 +11,18 @@ load_dotenv(find_dotenv(), override=True)
 
 
 class Config:
+    # Database setting
     DB_NAME: str = str(getenv("DB_NAME", "TEST"))
     DB_COLLECTION: str = str(getenv("DB_COLLECTION", "TEST_DB"))
     uri: str = str(getenv("MONGO_URI"))
+
+    # Github setting
     token: str = str(getenv("GITHUB_TOKEN"))
+    name_gh: str = str(getenv("NAME_GITHUB"))
+
+    # Flask setting
     PORT: int = int(getenv("PORT", 2011))
     allow_population_by_field_name = True
-    name_gh: str = str(getenv("NAME_GITHUB"))
 
     if not uri or not token or not name_gh:
         print(
