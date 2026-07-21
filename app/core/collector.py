9af0b8ce70f07106d2ac37ab186982284fecf581
Lai Van Sam <samvasang1192011@gmail.com>
2026-07-21 12:30:33 +0700

    I don't know to write this commit:)

--- app/core/collector.py
+++ app/core/collector.py
@@ -12,16 +12,21 @@ from app.database.connect_db import db
 from config import Config
 from requests import post
 from collections import defaultdict
-from typing import Dict, Any
+from typing import Dict, Any, Literal
 from concurrent.futures import ThreadPoolExecutor
 from concurrent.futures import as_completed
 from utils.logger import logger
+from github.Repository import Repository
 
 #TODO: Improve error handling by using specific exceptions instead of generic ones.
 
 def fetch_time_pushes_graphql(
     token: str, /, username: str
 ) -> tuple[dict[str, Any], int]:
+
+    pushes_data: int = 0
+    monthly_stats = defaultdict(int)
+
     try:
         url = "https://api.github.com/graphql"
         headers = {"Authorization": f"Bearer {token}"}
@@ -49,11 +54,7 @@ def fetch_time_pushes_graphql(
             print("Error from GitHub:", data, flush=True)
             return {}, 0
 
-        monthly_stats = defaultdict(int)
-        weeks = data["data"]["user"]["contributionsCollection"]["contributionCalendar"][
-            "weeks"
-        ]
-        pushes_data: int = 0
+        weeks = data["data"]["user"]["contributionsCollection"]["contributionCalendar"]["weeks"]
         for week in weeks:
             for day in week["contributionDays"]:
                 # The date format is 'YYYY-MM-DD', so we take the first 7 characters to get 'YYYY-MM'
@@ -62,6 +63,9 @@ def fetch_time_pushes_graphql(
                 pushes_data += int(day["contributionCount"])
 
         return dict(monthly_stats), pushes_data
+    except KeyError:
+        logger.error(f"KeyError occurred")
+        return {}, 0
     except TimeoutError:
         logger.error("Time out to connect Github")
         return {}, 0
@@ -70,10 +74,10 @@ def fetch_time_pushes_graphql(
         return {}, 0
 
 #OPTIMIZE: Need to replace this function with a direct request query to improve performance.
-def gets_info_repo(repo):
+def gets_info_repo(repo) -> tuple[int] | tuple[Any, Any, Any, int, int]:
     """This function handles data retrieval for a specific repository."""
     if repo.size == 0:
-        return 0, 0, 0, 0, 0
+        return *(0,) * 5,
 
     stars = repo.stargazers_count
     pulls = repo.get_pulls(state="all").totalCount
@@ -90,6 +94,7 @@ def get_github_stats() -> dict[str, int | dict[str, int]] | None:
         gh = get_client()
         user = gh.get_user(Config.NAME_GITHUB)
         repos = list(user.get_repos())
+
         #TODO: Proceed with developing the retrieval of remaining data for the schema.
         stats: Dict[str, int | Dict[str, int]] = {
             "Starred_Repos": int(user.get_starred().totalCount),  #
@@ -106,18 +111,21 @@ def get_github_stats() -> dict[str, int | dict[str, int]] | None:
             "reviews_comments": 0,
             "Code_Reviews": 0,
         }
-        with ThreadPoolExecutor(max_workers=20) as executor:
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
 
             future_pushes: Any = executor.submit(
                 fetch_time_pushes_graphql, Config.TOKEN, Config.NAME_GITHUB
             )
-            future_repos = {
+            future_repos: dict[Any, Repository] = {
                 executor.submit(gets_info_repo, repo): repo for repo in repos
             }
 
             for future in as_completed(future_repos):
                 #HACK: The code is prone to crashing because it uses [] instead of .get(), and lacks specific exception handling for potential indexing errors.
                 stars, pulls, issues_count, issues_comments, repo_fork = future.result()
+
+                # OPTIMIZE: We can optimize adding individual variables into a single block and use for k, v to merge the data.
                 stats["Stars_Earned"] += stars
                 stats["Pull_Requests"] += pulls
                 stats["Issues"] += issues_count
@@ -127,10 +135,15 @@ def get_github_stats() -> dict[str, int | dict[str, int]] | None:
             time_push, push_count = future_pushes.result()
             stats["Time_Pushes"] = time_push
             stats["pushes"] = push_count
+
         return stats
+
     except KeyError:
         #FIXME: Handling KeyError without addressing the root cause; may result in data inconsistencies.
         logger.error(f"KeyError occurred")
     except Exception as e:
         logger.error(f"An unexpected error occurred: {e}")
-print(get_github_stats())
+
+logger.info(get_github_stats())
+
+
